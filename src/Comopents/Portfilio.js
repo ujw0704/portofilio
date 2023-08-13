9660148bc361efaa23ed698516094a33826fd5b9
ujw0704 <ujwalsharma571@gmail.com>
2023-08-13 23:54:17 +0530

    one more

--- src/Comopents/Portfilio.js
+++ src/Comopents/Portfilio.js
@@ -6,23 +6,24 @@ function Portfilio() {
       <div className='heading'>
           <h1 className='port'>PORTFILIO</h1>
       </div>
+      <div className='images'> 
       <div className='img-cont'>
            <div className='first-img'>
             <img src='https://ananthukrish.netlify.app/assets/screenshot.66ae1500.png'alt></img>
            
            </div>
-           <div className='second -img'>
+           {/* <div className='second -img'>
      
            </div>
            <div className='third-img'>
      
-           </div>
+           </div> */}
 
 
 
            
       </div>
-      
+      </div>
 
 
       <div className='img-cont'>
