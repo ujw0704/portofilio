750f514d866d377666b8fe13bf03f4d503316646
ujw0704 <ujwalsharma571@gmail.com>
2023-08-18 12:40:54 +0530

    created few components

---
+++ src/Comopents/Education.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Qulifaction from './Qulifaction'
+function Education() {
+  return (
+    <div>
+          <div className='container'>
+       
+            <div className='date'>
+                <p>MAY,2016 - SEP,2020</p>
+            </div>
+
+            <div className='page'>
+
+               <h2>MAHAGHURU INSTITUTE OF TECHNOLOGY</h2>
+                
+               <h2>Bachelor's degree, Computer Science</h2>
+               <ul className='content'>
+           
+        <li>-Established in the year 2009, MAHAGURU INSTITUTE OF TECHNOLOGY (Formerly SVNCE), has been successful in producing distinct engineers over a decade. Since its inception our main aim is to deliver quality education and give our students the right exposure by maintaining international standards of technical education.</li>
+
+
+               </ul>
+            </div>
+               
+            
+        </div>
+          <div className='container'>
+       
+            <div className='date'>
+                <p>JUN,2014 - JUN,2016</p>
+            </div>
+
+            <div className='page'>
+                
+               <h2>BJSM MADATHIL</h2>
+                
+               <h2>Higher Secondary</h2>
+               <ul className='content'>
+           
+        <li>-Established in the year 2009, MAHAGURU INSTITUTE OF TECHNOLOGY (Formerly SVNCE), has been successful in producing distinct engineers over a decade. Since its inception our main aim is to deliver quality education and give our students the right exposure by maintaining international standards of technical education.</li>
+
+
+               </ul>
+            </div>
+               
+            
+        </div>
+    </div>
+  )
+}
+
+export default Education
--- src/Comopents/Work1.js
+++ src/Comopents/Work1.js
@@ -3,7 +3,9 @@ import './work.css'
 
 function Work1() {
   return ( 
-      <div className='wrapper'>
+    <>
+  
+      
         <div className='container'>
        
             <div className='date'>
@@ -12,7 +14,8 @@ function Work1() {
 
             <div className='page'>
                <h2>PANAPPS INC</h2>
-               <h3>Software Developer</h3>
+                
+               <h2>Software Developer</h2>
                <ul className='content'>
            
                 <li>- Develop, test and implement new software programs with vue js and dotnet</li>
@@ -27,10 +30,42 @@ function Work1() {
                
             
         </div>
+
+
+
+        <div className='container'>
+       
+       <div className='date'>
+           <p>JUN,2022 - PRESENT</p>
+       </div>
+
+       <div className='page'>
+          <h2>PANAPPS INC</h2>
+           
+          <h2>Software Developer</h2>
+          <ul className='content'>
+      
+           <li>- Develop, test and implement new software programs with vue js and dotnet</li>
+           <li>- Clearly and regularly communicate with management and technical support colleagues</li>
+           <li> - Design and update software database. This includes, but is not limited to: Software Applications, Web Sites, Data Communication Processes and User Interfaces</li>
+           <li>- Analyze user requirements and convert requirements to design documents</li>
+           <li>- Work with experienced team members to conduct root cause analysis of issues, review new and existing code and/or perform unit testing</li>
+
+
+          </ul>
+       </div>
+          
+       
+   </div>
+
+
+</>
+
+
            
                         
 
-                   </div>
+                   
              
      
   )
